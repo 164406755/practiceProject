8de3e933ac356be6d2eb36577bd99aad9f26808e
164406755 <164406755@qq.com>
2019-12-31 18:08:40 +0800

    practice demo create

---
+++ springboot/webdemo/src/test/java/com/study/springboot/webdemo/WebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
